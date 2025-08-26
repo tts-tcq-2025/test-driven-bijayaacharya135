24313d35bbcddefdb4ed74177766bc712c55cfef
bijayaacharya135 <internbijaya@gmail.com>
2025-08-26 08:46:45 +0530

    stringcalculator.h

--- stringcalculator.h
+++ stringcalculator.h
@@ -1,11 +1,17 @@
 #pragma once
 #include <string>
+#include <sstream>
 
 class StringCalculator {
 public:
     int Add(const std::string& numbers) {
         if (numbers.empty()) return 0;
-        // Further logic will be added in next steps
-        return 0;
+        int sum = 0;
+        std::stringstream ss(numbers);
+        std::string item;
+        while (std::getline(ss, item, ',')) {
+            sum += std::stoi(item);
+        }
+        return sum;
     }
 };
