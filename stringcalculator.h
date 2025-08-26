736e9cff6dd75916974a5f05b40a08ee6ccc1f23
bijayaacharya135 <internbijaya@gmail.com>
2025-08-26 08:38:41 +0530

    stringcalculator.h

---
+++ stringcalculator.h
@@ -0,0 +1,11 @@
+#pragma once
+#include <string>
+
+class StringCalculator {
+public:
+    int Add(const std::string& numbers) {
+        if (numbers.empty()) return 0;
+        // Further logic will be added in next steps
+        return 0;
+    }
+};
